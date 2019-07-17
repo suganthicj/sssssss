dc11008fb5e1d63f9a7784b14c23465848a4a104
suganthicj <52270901+suganthicj@users.noreply.github.com>
2019-07-16 20:49:20 -0700

    Create ssssss.py

---
+++ ssssss.py
@@ -0,0 +1,19 @@
+# Creating a String 
+# with double Quotes 
+String1 = "I'm a Geek"
+print("\nString with the use of Double Quotes: ") 
+print(String1) 
+  
+# Creating a String 
+# with triple Quotes 
+String1 = '''I'm a Geek and I live in a world of "Geeks"'''
+print("\nString with the use of Triple Quotes: ") 
+print(String1) 
+  
+# Creating String with triple 
+# Quotes allows multiple lines 
+String1 = '''Geeks 
+            For 
+            Life'''
+print("\nCreating a multiline String: ") 
+print(String1)
